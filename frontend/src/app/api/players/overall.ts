e12a6c19dedb7685a085ddc999919823bdcf986f
cstino <hcskso96@gmail.com>
2025-06-03 22:48:26 +0200

    fix: Rimosse tutte le API routes dal frontend per deploy

--- frontend/src/app/api/players/overall.ts
+++
@@ -1,3 +0,0 @@
-export function calcOverall({ ATT, DIF, VEL, PAS, FOR, POR }: { ATT: number, DIF: number, VEL: number, PAS: number, FOR: number, POR: number }) {
-  return Number(((ATT + DIF + VEL + PAS + FOR + POR) / 6).toFixed(2));
-} 
